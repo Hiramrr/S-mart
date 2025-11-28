3f3a47a3b7776eb1ed7e6a2ba1e42743b3bd0daf
TheBofoBM <zs23014140@estudiantes.uv.mx>
2025-11-27 20:40:15 -0600

    documentación store/counter.js

--- Frontend/src/stores/counter.js
+++ Frontend/src/stores/counter.js
@@ -1,9 +1,25 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+/**
+ * Un store de ejemplo para demostrar la funcionalidad básica de Pinia.
+ *
+ * @exports useCounterStore
+ */
 export const useCounterStore = defineStore('counter', () => {
+  /**
+   * El estado del contador.
+   * @type {import('vue').Ref<number>}
+   */
   const count = ref(0)
+  /**
+   * Un getter computado que devuelve el doble del valor del contador.
+   * @type {import('vue').ComputedRef<number>}
+   */
   const doubleCount = computed(() => count.value * 2)
+  /**
+   * Una acción que incrementa el valor del contador en uno.
+   */
   function increment() {
     count.value++
   }
