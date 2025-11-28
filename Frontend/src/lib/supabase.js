3435367c04aadedb2462a814463ca161f219e208
TheBofoBM <zs23014140@estudiantes.uv.mx>
2025-11-27 20:19:49 -0600

    documentación lib/supabase.js

--- Frontend/src/lib/supabase.js
+++ Frontend/src/lib/supabase.js
@@ -1,9 +1,28 @@
 import { createClient } from '@supabase/supabase-js'
 
+/**
+ * URL del proyecto de Supabase.
+ * @type {string}
+ */
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+/**
+ * Llave anónima pública de Supabase.
+ * @type {string}
+ */
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+/**
+ * URL de redirección después del inicio de sesión.
+ * @type {string}
+ */
 const redirectUrl = import.meta.env.VITE_REDIRECT_URL || window.location.origin
 
+/**
+ * Instancia del cliente de Supabase.
+ *
+ * @see https://supabase.com/docs/reference/javascript/create-client
+ *
+ * @type {import('@supabase/supabase-js').SupabaseClient}
+ */
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
