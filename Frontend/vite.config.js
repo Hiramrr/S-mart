4394f06eb8bfcab26600baadb6c857899c5445b0
SarahBaez <zS23014136@estudiantes.uv.mx>
2025-11-29 16:26:12 -0600

    Documentacion vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,3 +1,9 @@
+/**
+ * @file vite.config.js
+ * @description Archivo de configuración para Vite.
+ * Define los plugins utilizados (Vue, DevTools) y configura los alias de ruta
+ * (como '@' apuntando a 'src') para simplificar las importaciones en el proyecto.
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
