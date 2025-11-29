6c32c31929e0cea7006eecd21e2d2b224d29168e
SarahBaez <zS23014136@estudiantes.uv.mx>
2025-11-29 16:01:53 -0600

    Documentacion app

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,10 @@
 <script setup>
+/**
+ * @file App.vue
+ * @description Componente raíz de la aplicación.
+ * Actúa como el contenedor principal (layout base) que renderiza las vistas dinámicas
+ * gestionadas por el Vue Router a través de <RouterView>.
+ */
 import { RouterView } from 'vue-router'
 </script>
 
